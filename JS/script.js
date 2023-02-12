ccced591b23d37e66efa14e104285a0cf868e2fc
Tatiane Lopes <53845107+tati718@users.noreply.github.com>
2023-02-12 13:16:25 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,41 @@
+//console.log('Teste no console do javascript');
+
+function start(){
+    var buttonCalculateImc= document.querySelector('#button-calculate-imc');
+    buttonCalculateImc.addEventListener('click' ,handleButtonClick);
+
+    var inputWeight = document.querySelector('#input-weight');      
+    var inputHeight = document.querySelector('#input-height');
+
+
+    inputWeight.addEventListener('input', handleButtonClick);
+    inputHeight.addEventListener('input', handleButtonClick);
+
+    handleButtonClick();
+    
+    //console.log('start');
+
+    //console.log(calculateImc(98, 1.78)); chamei a função
+}
+
+
+function calculateImc(weight, height){
+    return weight /(height * height);
+}
+
+function handleButtonClick(){
+    var inputWeight = document.querySelector('#input-weight');        
+    var inputHeight = document.querySelector('#input-height');
+    var imcResult = document.querySelector('#imc-result');
+
+    var weight = Number(inputWeight.value);
+    var height = Number(inputHeight.value);
+    
+    var imc = calculateImc(weight, height);
+    var formattedImc = imc.toFixed(2).replace('.', ',');
+    
+    imcResult.textContent = formattedImc;
+    
+}
+
+start();
